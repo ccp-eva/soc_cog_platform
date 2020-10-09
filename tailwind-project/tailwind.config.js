f384e6bacbb89d02834bf93585f501392e9d9618
Julia Prein <jprein@me.com>
2020-10-09 14:35:41 +0200

    create exemplary tailwind project

---
+++ tailwind-project/tailwind.config.js
@@ -0,0 +1,15 @@
+// you can add fonts, colors etc
+// in terminal, run:
+// npx tailwind init // for configuration
+// npm run build:css // after adding some code, run again to refresh
+module.exports = {
+  theme: {
+    // is the same as <div class="container mx-auto px-2"> in html file
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  variants: {},
+  plugins: [],
+};
